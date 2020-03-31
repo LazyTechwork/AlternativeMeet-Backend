be8d4dae537db8b67a3b4fd5ff5b97b63dfeed79
LazyTechwork <lazytechwork@gmail.com>
2020-03-31 19:22:16 +0300

    Added docs

--- app/User.php
+++ app/User.php
@@ -6,6 +6,40 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property int $vk_id
+ * @property string $firstname
+ * @property string $lastname
+ * @property string|null $description
+ * @property mixed|null $geo
+ * @property string|null $birthday
+ * @property mixed|null $social
+ * @property string|null $avatar
+ * @property int $sex
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereBirthday($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereFirstname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereGeo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereLastname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereSex($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereSocial($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereVkId($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
