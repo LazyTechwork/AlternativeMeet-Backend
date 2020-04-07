af8d4636e767756832aa7c049bd3ef78c1b28358
LazyTechwork <lazytechwork@gmail.com>
2020-04-07 11:08:59 +0300

    Updated IDE docs

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 7.4.0 on 2020-04-07 07:11:28.
+ * Generated for Laravel 7.4.0 on 2020-04-07 08:08:46.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
